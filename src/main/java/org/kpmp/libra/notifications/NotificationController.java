56e8f6efd2e0c2d0bb0df82cfa2dcc6931c12002
Becky Reamy <rlreamy@umich.edu>
2019-06-21 16:04:16 -0400

    KPMP-1087: Configure to use the orion mongodb

--- src/main/java/org/kpmp/libra/notifications/NotificationController.java
+++ src/main/java/org/kpmp/libra/notifications/NotificationController.java
@@ -5,7 +5,9 @@ import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,13 +23,15 @@ public class NotificationController {
 		this.packageEventService = packageEventService;
 	}
 
-	@RequestMapping(value = "/v1/package/notifications", method = RequestMethod.POST)
-	public @ResponseBody PackageNotificationEvent notifyNewPackage(String packageId, String packageType,
-			Date datePackageSubmitted, String submitterName) {
-		log.info("URI: {} | MSG: {}", "/v1/package/notifications", "Adding notification for PKGID: " + packageId);
+	@RequestMapping(value = "/v1/notifications/package/{packageId}/packageType/{packageType}/packageSubmitted/{datePackageSubmitted}/submitter/{submitter}", method = RequestMethod.POST)
+	public @ResponseBody PackageNotificationEvent notifyNewPackage(@PathVariable String packageId,
+			@PathVariable String packageType,
+			@PathVariable("datePackageSubmitted") @DateTimeFormat(pattern = "yyyyMMdd") Date datePackageSubmitted,
+			@PathVariable String submitter) {
+		log.info("URI: {} | MSG: {}", "/v1/notifications", "Adding notification for PKGID: " + packageId);
 
 		PackageNotificationEvent event = packageEventService.saveNotifyEvent(packageId, packageType,
-				datePackageSubmitted, submitterName);
+				datePackageSubmitted, submitter);
 
 		return event;
 	}
