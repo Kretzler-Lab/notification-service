371b1a6ac141a8158a8b3e1a5f5ab1515975b2b3
Zach Wright <zwright@umich.edu>
2024-06-14 13:55:53 -0400

    IP-456: Restructure

--- src/main/java/org/kpmp/eridanus/notifications/PackageRepository.java
+++
@@ -1,9 +0,0 @@
-package org.kpmp.eridanus.notifications;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PackageRepository extends MongoRepository<Package, String> {
-
-	public Package findByPackageId(String packageId);
-
-}
